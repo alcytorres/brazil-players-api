35e945bd10889a83095dfd6949d5d299696c4af8
Alcy Torres <mxalcytorres@gmail.com>
2024-05-27 17:31:06 -0400

    Add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+player = Player.new(name: "Zico", position: "Midfielder", nationality: "Brazil", dob: 1953)
+player.save
+player = Player.new(name: "Romario", position: "Forward", nationality: "Brazil", dob: 1966)
+player.save
+player = Player.new(name: "Ronaldo", position: "Forward", nationality: "Brazil", dob: 1976)
+player.save
+player = Player.new(name: "Ronaldinho", position: "Midfielder", nationality: "Brazil", dob: 1980)
+player.save
+player = Player.new(name: "Kaka", position: "Midfielder", nationality: "Brazil", dob: 1982)
+player.save
